eefb306ec1a5b5188952cb4a4a982a942887752b
mstagnitti <monique@learntoplaytherapy.com>
2025-03-06 10:55:16 +1100

    Added the search functionality using the API

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityTemperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(cityTemperature);
+
+  let currentCity = document.querySelector("#city");
+  currentCity.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "aa50c5taa349ad0ccb0b41133of3fd60";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit="metric"`;
+
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function newCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#city-input");
-  let currentCity = document.querySelector("#city");
-  currentCity.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", newCity);
+
+searchCity("Melbourne");
 
 let day = document.querySelector("#date");
